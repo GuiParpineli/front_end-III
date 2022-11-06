81c376a6b603ac7889fabe82a4969ed7831377fd
Gui Parpineli <guiparpineli@GuiPC>
2022-11-06 16:05:08 -0300

    add setima aula

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {QuartaAula} from "./lessons/QuartaAula";
 import {QuintaAula} from "./lessons/QuintaAula";
 import {NewGames} from "./pages/NewGames";
 import {SetimaAula} from "./lessons/SetimaAula";
+import {OitavaAula} from "./lessons/OitavaAula";
 
 
 function App() {
--- src/lessons/SetimaAula/index.jsx
+++ src/lessons/SetimaAula/index.jsx
@@ -1,30 +1,113 @@
 import './style.scss'
 import {useState} from "react";
-
+import {SetimaAulaCardProduto} from "../../components/SetimaAulaCardProduto/index.jsx";
 export function SetimaAula() {
 
-    const [contador, setContador] = useState(0)
-    const [temaEscuro, setTemaEscuro] = useState(true)
+    const [nomeProduto, setNomeProduto] = useState('')
+    const [precoProduto, setPrecoProduto] = useState('')
+    const [fotoProduto, setFotoProduto] = useState('')
+    const [formularioErro, setFormularioErro] = useState(false)
+    const [allProducts, setAllProducts] = useState([
+        {
+            id: 1,
+            name: 'Xbox',
+            price: '3.000',
+            picture: 'https://http2.mlstatic.com/D_NQ_NP_695945-MLB50098260025_052022-O.webp'
+        },
+        {
+            id: 2,
+            name: 'Playstation 5',
+            price: '5.000',
+            picture: 'https://http2.mlstatic.com/D_NQ_NP_841787-MLA44484414455_012021-O.webp'
+        },
+        {
+            id: 3,
+            name: 'Switch',
+            price: '2.000',
+            picture: 'https://http2.mlstatic.com/D_NQ_NP_771705-MLA40692342174_022020-W.webp'
+        }
+    ])
 
-    function somarNumero() {
-        setContador(contador + 1)
-    }
+    function cadastrarProduto(event) {
 
-    function mudarTema() {
-        setTemaEscuro(!temaEscuro)
+        event.preventDefault()
+
+        const novoProdutoCadastrado = {
+            name: nomeProduto,
+            price: precoProduto,
+            picture: fotoProduto
+        }
+
+        if (nomeProduto === '' || precoProduto === '') {
+
+            setFormularioErro(true)
+
+        } else {
+
+            setFormularioErro(false)
+
+            setAllProducts([...allProducts, novoProdutoCadastrado])
+
+            setNomeProduto('')
+            precoProduto('')
+            fotoProduto('')
+
+        }
     }
 
     return (
 
-        <main className={`setima-aula-component ${temaEscuro ? "dark-theme" : ""}`}>
+        <main className="setima-aula-component">
 
-            <h1>Contador</h1>
+            <div className="tittle-wrapper">
+                <h1>Produtos</h1>
+                {/* <button onClick={addNewProduct}>Adicionar novo produto</button> */}
+            </div>
 
-            <button onClick={() => mudarTema()}>Mudar Tema</button>
-            <button onClick={somarNumero}>Adicionar</button>
-            <span>Número atual: {contador}</span>
+            <form className={formularioErro ? 'form-error' : ''} onSubmit={event => cadastrarProduto(event)}>
+
+                <div>
+                    <label htmlFor="nomeProduto">Nome</label>
+                    <input id="nomeProduto" type="text" value={nomeProduto} onChange={event => setNomeProduto(event.target.value)} />
+                </div>
+
+                <div>
+                    <label htmlFor="precoProduto">Preço</label>
+                    <input id="precoProduto" type="text" value={precoProduto} onChange={event => setPrecoProduto(event.target.value)} />
+                </div>
+
+                <div>
+                    <label htmlFor="fotoProduto">Foto</label>
+                    <input id="fotoProduto" type="text" value={fotoProduto} onChange={event => setFotoProduto(event.target.value)} />
+                </div>
+
+                <button type='submit'>Cadastrar Produto</button>
+                <button type='reset'>Limpar formulário</button>
+
+            </form>
+
+            {
+                formularioErro ? (
+                    <span>O seu formulário contem erros</span>
+                ) : null
+            }
+
+            <section className='products'>
+                {
+                    allProducts.map(
+                        product => {
+                            return (
+                                <SetimaAulaCardProduto
+                                    productData={product}
+                                />
+                            )
+                        }
+                    )
+                }
+            </section>
 
         </main>
 
     )
+
 }
